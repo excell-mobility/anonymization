46a0fe43bd7d1e945fdd306af296c2bc6f6b9113
Felix Kunde <felix-kunde@gmx.de>
2018-03-27 14:39:05 +0200

    minor update to main class

--- src/main/java/anonymization/Application.java
+++ src/main/java/anonymization/Application.java
@@ -54,8 +54,6 @@ public class Application {
           .build()
           .genericModelSubstitutes(ResponseEntity.class)
           .protocols(Sets.newHashSet("https"))
-//          .host("localhost:45555")
-//          .host("141.64.5.234/excell-anonymization-api")
           .host("dlr-integration.minglabs.com")
           .securitySchemes(Lists.newArrayList(apiKey()))
           .securityContexts(Lists.newArrayList(securityContext()))
@@ -107,8 +105,8 @@ public class Application {
         		  "Beuth Hochschule für Technik Berlin - Labor für Rechner- und Informationssysteme - MAGDa Gruppe",
         		  "https://projekt.beuth-hochschule.de/magda/poeple",
         		  "spieper@beuth-hochschule"),
-          "Apache 2",
-          "http://www.apache.org/licenses/LICENSE-2.0",
+          "Link to source code",
+          "https://github.com/excell-mobility/anonymization",
           new ArrayList<VendorExtension>());
         return apiInfo;
     }
